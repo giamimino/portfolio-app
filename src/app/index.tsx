3a28d20c80796eabd29871931029fc67ad1c63b5
giamimino <miminoshvili2102@gmail.com>
2026-09-22 19:41:45 +0400

    chore: setup NativeWind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
