24bf361c8436f13f8803b9fae934338333c77605
giamimino <miminoshvili2102@gmail.com>
2026-09-25 01:22:23 +0400

    fix(issue-pr-automation): provide file sha when updating issue file

--- .github/scripts/issue-pr-automation.js
+++ .github/scripts/issue-pr-automation.js
@@ -60,6 +60,13 @@ module.exports = async ({ github, context }) => {
 
         // step 3 Create initial commit
 
+        const { data: existingFile } = await github.rest.repos.getContent({
+          owner,
+          repo,
+          path: 'ISSUE.md',
+          ref: branch,
+        })
+
         await github.rest.repos.createOrUpdateFileContents({
           owner: owner,
           repo: repo,
@@ -67,6 +74,7 @@ module.exports = async ({ github, context }) => {
           message: 'Initialize issue branch',
           content: Buffer.from(body).toString('base64'),
           branch,
+          sha: existingFile.sha,
           committer: {
             name: 'github-actions[bot]',
             email: '41898282+github-actions[bot]@users.noreply.github.com',
